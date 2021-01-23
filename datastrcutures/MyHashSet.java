0267f786c9f7b647438aac02f563506ade842015
Bruno Costa <bcosta76@gmail.com>
2021-01-23 22:33:04 -0100

    Adding MyHashSet again

---
+++ datastrcutures/MyHashSet.java
@@ -0,0 +1,47 @@
+// Sijmple HashSet implementation
+// Need todo implement rehashing
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyHashSet {
+    private final int MAX_VALUE = 1_000_000; // no value stored in the hashset can be bigger than this
+    private final int ARRAY_SIZE = 100; // my bucket list size
+    private List<List<Integer>> parentList; // parent list is my bucket list. every bucket has a linked list inside
+
+    public HashSet() {
+        parentList = new ArrayList<>(ARRAY_SIZE);
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            parentList.add(null);
+        }
+    }
+
+    public void add(int key) {
+        int idex = key % ARRAY_SIZE; // mapping it to a bucket in my parent list
+        List<Integer> childList = parentList.get(index);
+        if(childList == null) {
+            childList = new LinkedList<>();
+            list.add(key);
+            parentList.set(index, childList);
+        }
+        else {
+            if (!childList.contains(key)) {
+                childList.add(key);
+            }
+        }
+    }
+
+    public void remove(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        if (childList != null) {
+            childList.remove(Integer.valueOf(key)); // remove the value, not the position 
+        }
+    }
+
+    public boolean contains(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        return childList != null && childList.contains(key);
+    }
+}
