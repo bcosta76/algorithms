3c7b32ec766caf9ac836ea6475967a559867da19
Bruno Costa <bcosta76@gmail.com>
2021-01-23 18:52:02 -0100

    Finished DataStructures/MyHashSet

--- DataStructures/MyHashSet.java
+++ DataStructures/MyHashSet.java
@@ -1,3 +1,8 @@
+// Sijmple HashSet implementation
+// Need todo implement rehashing
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MyHashSet {
     private final int MAX_VALUE = 1_000_000; // no value stored in the hashset can be bigger than this
@@ -9,5 +14,34 @@ public class MyHashSet {
         for (int i = 0; i < ARRAY_SIZE; i++) {
             parentList.add(null);
         }
+    }
+
+    public void add(int key) {
+        int idex = key % ARRAY_SIZE; // mapping it to a bucket in my parent list
+        List<Integer> childList = parentList.get(index);
+        if(childList == null) {
+            childList = new LinkedList<>();
+            list.add(key);
+            parentList.set(index, childList);
+        }
+        else {
+            if (!childList.contains(key)) {
+                childList.add(key);
+            }
+        }
+    }
+
+    public void remove(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        if (childList != null) {
+            childList.remove(Integer.valueOf(key)); // remove the value, not the position 
+        }
+    }
+
+    public boolean contains(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        return childList != null && childList.contains(key);
     }
 }
